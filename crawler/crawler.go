3e3a4d823d4b2f5ad2a9fe1e3bb215fa2e7c0ec4
ZacharyGroff <groff.zackm@gmail.com>
2019-07-05 08:03:19 -0400

    add crawler skeleton

---
+++ crawler/crawler.go
@@ -0,0 +1,25 @@
+package crawler
+
+import (
+	"log"
+	"github.com/ZacharyGroff/Shelob/config"
+	"github.com/ZacharyGroff/Shelob/scheduler"
+)
+
+type Crawler struct {
+	config *config.Config
+	scheduler *scheduler.Scheduler
+}
+
+func NewCrawler(config *config.Config, scheduler *scheduler.Scheduler) Crawler {
+	return Crawler{config, scheduler}
+}
+
+func (crawler Crawler) Start() {
+	log.Printf("Starting Shelob...\n")
+}
+
+func (crawler Crawler) Stop() {
+	log.Printf("Starting Shelob...\n")
+
+}
