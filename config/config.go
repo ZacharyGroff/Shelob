40b23a339fee0d1477a91a94ee271179e497f0a4
ZacharyGroff <groff.zackm@gmail.com>
2019-07-05 08:02:33 -0400

    pull over config package

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"encoding/json"
+	"os"
+	"log"
+	"io/ioutil"
+)
+
+type Config struct {
+	SeedPath string `json:"seedPath"`
+}
+
+func (config *Config) parseConfig(path string) error {
+	file, err := os.Open(path)
+	defer file.Close()
+	byteValue, _ := ioutil.ReadAll(file)
+	json.Unmarshal(byteValue, &config)
+	
+	return err
+}
+
+func NewConfig() *Config {
+	config := Config{}
+	err := config.parseConfig("config/conf.json")
+	
+	if err != nil {
+		log.Fatal("Error while parsing config")
+	}
+	
+	return &config
+}
