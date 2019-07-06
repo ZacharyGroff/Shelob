f5c41467e1b0b37e9cb683ef5dd0bab8de843da5
ZacharyGroff <groff.zackm@gmail.com>
2019-07-06 10:57:16 -0400

    update scheduler with queue

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -2,26 +2,73 @@ package scheduler
 
 import (
 	"log"
+	"os"
+	"bufio"
 	"net/url"
 	"github.com/ZacharyGroff/Shelob/config"
+	"github.com/ZacharyGroff/Shelob/queue"
 )
 
 type Scheduler struct {
-	seeds []url.URL
+	config *config.Config
+	seedQueue *queue.SeedQueue
 }
 
-func NewScheduler(config *config.Config) *Scheduler {
-	seeds := getInitialSeeds(config.SeedPath)
-	return &Scheduler{seeds}
+func NewScheduler(c *config.Config, q *queue.SeedQueue) *Scheduler {
+	return &Scheduler{c, q}
 }
 
-func (scheduler Scheduler) Schedule() {
-	log.Printf("Scheduling...\n")
+func (scheduler Scheduler) Start() {
+	log.Printf("Loading initial seeds...\n")
+	err := scheduler.LoadInitialSeeds()
+	if err != nil {
+		log.Fatalf("Failed to load initial seeds with error %s\n", error.Error)
+	}
 }
 
-func getInitialSeeds(path string) []url.URL {
+func (scheduler Scheduler) LoadInitialSeeds() error {
+	lines, err := getFileLines(scheduler.config.SeedPath)
+	if err != nil {
+		return err
+	}
+
+	initialSeeds, err := parseFileLines(lines)
+	if err != nil {
+		return err
+	}
+
+	for _, seed := range initialSeeds {
+		scheduler.seedQueue.Put(seed)
+	}
+
+	return nil
+}
+
+func getFileLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines, nil
+}
+
+func parseFileLines(lines []string) ([]url.URL, error) {
 	var urls []url.URL
-	u, _ := url.Parse("https://google.com/")
-	urls = append(urls, *u)
-	return urls
+	for _, line := range lines {
+		url, err := url.Parse(line)
+		if err != nil {
+			return nil, err
+		}
+		urls = append(urls, *url)
+	}
+	
+	return urls, nil
 }
