b216b51e7c65543266d256c234ac035999184454
ZacharyGroff <groff.zackm@gmail.com>
2019-07-07 14:49:17 -0400

    update config tests

--- config/config_test.go
+++ config/config_test.go
@@ -5,35 +5,13 @@ import (
 	"strings"
 )
 
-func TestParseConfigPort(t *testing.T) {
+func TestParseSeedPath(t *testing.T) {
 	config := Config{}
 	config.parseConfig("conf_test.json")
 	
-	expected := ":42"
-	actual := config.Port
+	expected := "seedPathTest"
+	actual := config.SeedPath
 	if strings.Compare(expected, actual) != 0 {
 		t.Errorf("Expected: %s\nActual: %s\n", expected, actual)
-	}
-}
-
-func TestParseConfigRoute(t *testing.T) {
-	config := Config{}
-	config.parseConfig("conf_test.json")
-	
-	expected := "/test"
-	actual := config.Route
-	if strings.Compare(expected, actual) != 0 {
-		t.Errorf("Expected: %s\nActual: %s\n", expected, actual)
-	}
-}
-
-func TestParseConfigValidate(t *testing.T) {
-	config := Config{}
-	config.parseConfig("conf_test.json")
-	
-	expected := true
-	actual := config.Validate
-	if expected != actual {
-		t.Errorf("Expected: %t\nActual: %t\n", expected, actual)
 	}
 }
