4397c1d622de33c39c0e102bc5f55dc9d08b644d
ZacharyGroff <groff.zackm@gmail.com>
2019-07-08 12:40:46 -0400

    add flush

--- queue/seed_queue.go
+++ queue/seed_queue.go
@@ -1,19 +1,22 @@
 package queue
 
 import (
+	"bufio"
 	"errors"
 	"fmt"
+	"os"
 	"net/url"
 	"github.com/ZacharyGroff/Shelob/config"
 )
 
 type SeedQueue struct {
 	seeds chan url.URL
+	config *config.Config
 }
 
 func NewSeedQueue(config *config.Config) *SeedQueue {
 	seeds := make(chan url.URL, config.SeedBuffer)
-	return &SeedQueue{seeds}
+	return &SeedQueue{seeds, config}
 }
 
 func (q SeedQueue) Get() (url.URL, error) {
@@ -36,4 +39,26 @@ func (q SeedQueue) Put(url url.URL) error {
 		err := fmt.Errorf("No room in buffer. Discarding url: %+v\n", url)
 		return err
 	}
+}
+
+func (q SeedQueue) Flush() error {
+	file, err := os.OpenFile(q.config.SeedPath, os.O_WRONLY, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	initialSize := len(q.seeds)
+	for i := 0; i < initialSize; i++ {
+		url, err := q.Get()
+		if err != nil {
+			return err
+		}
+
+		fmt.Fprintln(writer, url) 
+	}
+	
+	return writer.Flush()
 }
