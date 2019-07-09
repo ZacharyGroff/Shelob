c0203254fbfcec1b61053368c6e887085c7758f4
ZacharyGroff <groff.zackm@gmail.com>
2019-07-09 13:05:26 -0400

    add basic request parsing for urls

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -4,7 +4,11 @@ import (
 	"log"
 	"os"
 	"bufio"
+	"io"
+	"fmt"
 	"net/url"
+	"net/http"
+	"golang.org/x/net/html"
 	"github.com/ZacharyGroff/Shelob/config"
 	"github.com/ZacharyGroff/Shelob/queue"
 )
@@ -24,6 +28,83 @@ func (scheduler Scheduler) Start() {
 	if err != nil {
 		log.Fatalf("Failed to load initial seeds with error %s\n", err.Error())
 	}
+}
+
+func (scheduler Scheduler) downloadAndParse(url url.URL) {
+	reader, err := download(url)
+	if err != nil {
+		return
+	}
+
+	childUrls, err := parse(reader)
+	if err != nil {
+		return
+	}
+
+	for _, childUrl := range childUrls {
+		scheduler.queue.Put(childUrl)
+	}
+}
+
+func download(url url.URL) (io.Reader, error) {
+	response, err := http.Get(url.String())
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Body, nil
+}
+
+func parse(reader io.Reader) ([]url.URL, error) {
+	tokenizer := html.NewTokenizer(reader)
+	urls, err := getUrls(tokenizer)
+	if err != nil {
+		return nil, err
+	}
+
+	return urls, nil
+}
+
+func getUrls(tokenizer *html.Tokenizer) ([]url.URL, error) {
+	var urls []url.URL
+	for {
+		tokenType := tokenizer.Next()
+		switch {
+		case tokenType == html.StartTagToken:
+			token := tokenizer.Token()
+			if isAnchor(token) {
+				url, err := parseAnchorToken(token)
+				if err != nil {
+					return nil, err
+				}
+				urls = append(urls, url)
+			}
+		case tokenType == html.ErrorToken:
+			return urls, nil
+		}
+	}
+	err := fmt.Errorf("Reached end of tokenenizer without ErrorToken. Tokenizer: %+v\n", tokenizer)
+
+	return nil, err
+}
+
+func isAnchor(token html.Token) bool {
+	return token.Data == "a"
+}
+
+func parseAnchorToken(token html.Token) (url.URL, error) {
+	for _, attr := range token.Attr {
+		if attr.Key == "href" {
+			u, err := url.Parse(attr.Val)
+			if err != nil {
+				return url.URL{}, err
+			}
+			return *u, nil
+		}
+	}
+	
+	err := fmt.Errorf("Attempted to parse anchor token with no href. Token: %+v\n", token)
+	return url.URL{}, err
 }
 
 func (scheduler Scheduler) loadInitialSeeds() error {
