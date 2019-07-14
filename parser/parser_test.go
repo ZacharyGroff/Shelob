c817583c0f604c13c5c5e48d9bbf4baf1c4291c2
ZacharyGroff <groff.zackm@gmail.com>
2019-07-14 12:14:01 -0400

    add fillInPartialUrl test

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,7 +1,9 @@
 package parser
 
 import (
+	"strings"
 	"testing"
+	"net/url"
 	"golang.org/x/net/html"
 )
 
@@ -32,5 +34,20 @@ func TestParseAnchorTokenFailure(t *testing.T) {
 	_, err := parseAnchorToken(token)
 	if err == nil {
 		t.Errorf("Expected error but nil returned.")
+	}
+}
+
+func TestFillInPartialLinks(t *testing.T) {
+	expectedUrl, _ := url.Parse("https://parent.com/test/path")
+	expected := expectedUrl.String()
+
+	partialUrl, _ := url.Parse("/test/path")
+	partialUrls := []url.URL{*partialUrl}
+	parent, _ := url.Parse("https://parent.com/")
+	completeUrls := fillInPartialLinks(partialUrls, *parent)
+	actual := completeUrls[0].String()
+
+	if strings.Compare(expected, actual) != 0 {
+		t.Errorf("Expected: %s\nActual: %s\n", expected, actual)
 	}
 }
